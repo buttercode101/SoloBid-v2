b696463163d3832561af73186b74560a7766f1d4
TheRealButter <162381975+TheRealButter@users.noreply.github.com>
2026-06-26 23:31:24 +0200

    feat: add dashboard actions wrapper

---
+++ src/pages/DashboardWithActions.tsx
@@ -0,0 +1,6 @@
+import Dashboard from './Dashboard';
+import { DueTodayActionsPanel } from '../components/duetoday/DueTodayActionsPanel';
+
+export default function DashboardWithActions() {
+  return <><Dashboard /><DueTodayActionsPanel /></>;
+}
